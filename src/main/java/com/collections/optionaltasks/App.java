cfecda152c2c3313d9954a58154e0c0964e43b70
Sergey Guzov <guzov1993@gmail.com>
2024-09-02 16:45:24 +0200

    Initial commit

---
+++ src/main/java/com/collections/optionaltasks/App.java
@@ -0,0 +1,10 @@
+package com.collections.optionaltasks;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
